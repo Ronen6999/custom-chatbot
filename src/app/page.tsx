b4e6f0d82bdc945adc681e27c2344aadc7006cde
Ronen <101617329+Ronen6999@users.noreply.github.com>
2024-05-08 21:38:57 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import LeftSection from "@/components/LeftSection";
 import RightSection from "@/components/RightSection";
 
 export default function Home() {
